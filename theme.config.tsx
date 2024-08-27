362b439aa5f4f2af75be8892e4012839ccd7b483
Tong Zhaoqi <zhaoqi.tong@outlook.com>
2024-08-27 15:30:54 +0800

    upload

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,6 +7,9 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/tzq0301/docs',
   },
   // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  head: (
+    <link rel="icon" type="image/x-icon" href="https://tzq0301.github.io/icon/TZQ-icon.ico"></link>
+  ),
   footer: {
     text: 'Docs about coding',
   },
